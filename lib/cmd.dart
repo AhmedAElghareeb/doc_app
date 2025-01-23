448670a6d06a6ac47041a404164b57a92f03f5bf
AhmedElghareebGeex <ahmed.elghareeb@geexar.dev>
2025-01-23 15:00:46 +0200

    Handle release flavor

--- lib/cmd.dart
+++ lib/cmd.dart
@@ -3,4 +3,5 @@
 //flutter run --debug -t lib/main_development.dart --flavor development
 //flutter run --debug -t lib/main_production.dart --flavor production
 
+//flutter build apk --release --flavor development --target lib/main_development.dart --no-tree-shake-icons
 //flutter build apk --release --flavor production --target lib/main_production.dart --no-tree-shake-icons
